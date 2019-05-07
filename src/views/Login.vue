669771b76fe30c80d650a08ca329d503ba491a6f
Jaime Loeuf <jaimeloeuf@gmail.com>
2019-05-08 01:04:22 +0800

    Created basic Login view.

---
+++ src/views/Login.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="login">
+    <h3>Sign In</h3>
+    <input type="text" placeholder="Email">
+    <br>
+    <input type="password" placeholder="Password">
+    <br>
+    <button>Login!</button>
+    <!-- @Todo fix the create new account link -->
+    <p>
+      Don't have an account yet? Create
+      <a href="#">here</a> now!
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "login",
+  data() {
+    return {};
+  },
+  methods: {}
+};
+</script>
+
+<style scoped>
+</style>
