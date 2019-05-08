7c86c80a39e6d9cbbffbebf9b3c6878384fa5a02
Jaime Loeuf <jaimeloeuf@gmail.com>
2019-05-08 13:25:31 +0800

    Renamed the Home view to Notes view: 1.	Renamed the file to Notes view instead of Home view
    2.	Changed the link names in the App nav bar
    
    3.	Updated the router to use the new view

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
+      <router-link to="/">Notes</router-link> |
       <router-link to="/about">About</router-link>
     </div>
     <div id="nav">
