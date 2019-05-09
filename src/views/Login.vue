d26eb09a3019c932b4f4ed8563313506085f2602
Jaime Loeuf <jaimeloeuf@gmail.com>
2019-05-09 13:00:02 +0800

    Updated the login view: 1.	Added SCSS into the view file
    2.	Added some basic code to start using firebase as app backend

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,28 +1,69 @@
 <template>
   <div class="login">
+    <img src="../assets/logo.png" alt="doVue logo">
     <h3>Sign In</h3>
-    <input type="text" placeholder="Email">
+    <input type="text" v-model="email" placeholder="Email" autofocus>
     <br>
-    <input type="password" placeholder="Password">
+    <input type="password" v-model="password" placeholder="Password">
     <br>
-    <button>Login!</button>
-    <!-- @Todo fix the create new account link -->
+    <button @click="login">Login</button>
     <p>
-      Don't have an account yet? Create
-      <a href="#">here</a> now!
+      Don't have an account yet? Hurry and
+      <router-link to="/sign-up">create one</router-link>now!
     </p>
   </div>
 </template>
 
 <script>
+//   import firebase from 'firebase';
+
 export default {
   name: "login",
   data() {
-    return {};
+    return {
+      email: "",
+      password: ""
+    };
   },
-  methods: {}
+  methods: {
+    login: function() {
+      firebase
+        .auth()
+        .signInWithEmailAndPassword(this.email, this.password)
+        .then(
+          user => {
+            this.$router.replace("home");
+          },
+          err => {
+            alert("Oops. " + err.message);
+          }
+        );
+    }
+  }
 };
 </script>
 
 <style scoped>
+/* "scoped" attribute limit the CSS to this component only */
+.login {
+  margin-top: 40px;
+}
+input {
+  margin: 10px 0;
+  width: 20%;
+  padding: 15px;
+}
+button {
+  margin-top: 20px;
+  width: 10%;
+  cursor: pointer;
+}
+p {
+  margin-top: 40px;
+  font-size: 13px;
+}
+p a {
+  text-decoration: underline;
+  cursor: pointer;
+}
 </style>
