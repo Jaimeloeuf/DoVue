4869c4af9f05176b2c7882da97240f302f975553
Jaime Loeuf <jaimeloeuf@gmail.com>
2019-05-10 11:14:55 +0800

    Added in firebase related items: 1.	Added the firebase config object into the main.js module
    2.	Added imports of the firebase package in both the login and signup view

--- src/main.js
+++ src/main.js
@@ -1,9 +1,23 @@
 import Vue from 'vue'
 import App from './App.vue'
+import firebase from 'firebase';
 import router from './router'
 import store from './store'
 
 Vue.config.productionTip = false
+
+// firebaseConfig auto generated in project settings
+const config = {
+    apiKey: "AIzaSyBLNzTnm890qIYStxM5_nljMh3CoxkVyDk",
+    authDomain: "dovue-c1892.firebaseapp.com",
+    databaseURL: "https://dovue-c1892.firebaseio.com",
+    projectId: "dovue-c1892",
+    storageBucket: "dovue-c1892.appspot.com",
+    messagingSenderId: "9521872878",
+    appId: "1:9521872878:web:e90626e43f23617a"
+  };
+
+firebase.initializeApp(config);
 
 new Vue({
   router,
