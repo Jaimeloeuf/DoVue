72da959e82ed5c5f1a74350e5a75337f8ee59c7e
Jaime Loeuf <jaimeloeuf@gmail.com>
2019-05-10 11:30:04 +0800

    Update to the routes: 1.	Added missing semi-colons in the import statements.
    2.	Corrected the formatting of the whole module.
    
    3.	Added the Login view as the default view.
    
    4.	Added a auto redirect for all unknown routes to the login view
    
    5.	Made the notes route to be protected with Auth meta value

--- src/router.js
+++ src/router.js
@@ -1,35 +1,50 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import Notes from './views/Notes.vue'
-import Login from './views/Login.vue'
-import Signup from './views/Signup.vue'
+import Vue from 'vue';
+import Router from 'vue-router';
 
-Vue.use(Router)
+import Notes from './views/Notes.vue';
+import Login from './views/Login.vue';
+import Signup from './views/Signup.vue';
+
+Vue.use(Router);
 
 export default new Router({
-  routes: [
-    {
-      path: '/notes',
-      name: 'notes',
-      component: Notes
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-    },
-    {
-        path: '/login',
-        name: 'login',
-        component: Login
-    },
-    {
-        path: '/sign-up',
-        name: 'signup',
-        component: Signup
-    }
-  ]
-})
+    routes: [
+        {
+            // Redirect any unknown routes to the login view
+            path: '*',
+            redirect: '/login'
+        },
+        {
+            // Set login view as the default view
+            path: '/',
+            redirect: '/login'
+        },
+        {
+            path: '/notes',
+            name: 'notes',
+            component: Notes,
+            meta: {
+                // The notes app is only available after authentication
+                requiresAuth: true
+            }
+        },
+        {
+            path: '/about',
+            name: 'about',
+            // route level code-splitting
+            // this generates a separate chunk (about.[hash].js) for this route
+            // which is lazy-loaded when the route is visited.
+            component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+        },
+        {
+            path: '/login',
+            name: 'login',
+            component: Login
+        },
+        {
+            path: '/sign-up',
+            name: 'signup',
+            component: Signup
+        }
+    ]
+});
