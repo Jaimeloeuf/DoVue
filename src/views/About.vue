6fa28ab02cb078810fad9a14602bee110861a821
Jaime Loeuf <jaimeloeuf@gmail.com>
2019-05-12 00:56:46 +0800

    Updated about view with more details

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,37 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <PublicNavbar/>
+    <h1>This is doVue</h1>
+    <h3 class="slogan">Simple, Reliable, Beautiful</h3>
+    <img src="@/assets/logo.png" alt="doVue logo">
+    <p class="description">
+      A todo and notes taking app, focused to help you tackle whatever is on hand.
+      We believe in keeping things Simple and functional, and so reliable that you never want to go back to whatever you were using before.
+    </p>
   </div>
 </template>
+
+
+<script>
+import PublicNavbar from "@/components/PublicNavbar.vue";
+
+export default {
+  name: "about",
+  components: {
+    PublicNavbar
+  }
+};
+</script>
+
+
+<style scoped>
+.about {
+  margin-top: 4em;
+}
+.slogan {
+}
+.description {
+  display: block;
+  max-width: 30em;
+}
+</style>
