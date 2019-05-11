f81e0eda1dd0d626e9b1573e5b665e23fd6ab266
Jaime Loeuf <jaimeloeuf@gmail.com>
2019-05-11 15:05:05 +0800

    Updated template and CSS of the login view

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="login">
     <img src="@/assets/logo.png" alt="doVue logo">
-    <h3>Sign In</h3>
+    <h3>Glad to have you back :)</h3>
     <input type="text" v-model="email" placeholder="Email" autofocus>
     <br>
     <input type="password" v-model="password" placeholder="Password">
@@ -9,7 +9,7 @@
     <button @click="login">Login</button>
     <p>
       Don't have an account yet? Hurry and
-      <router-link to="/sign-up">create one now!</router-link>
+      <router-link to="/sign-up">create one now</router-link>!
     </p>
   </div>
 </template>
@@ -40,21 +40,21 @@ export default {
 <style scoped>
 /* "scoped" attribute limit the CSS to this component only */
 .login {
-  margin-top: 40px;
+  margin-top: 4em;
 }
 input {
-  margin: 10px 0;
+  margin: 1em 0;
   width: 20%;
-  padding: 15px;
+  padding: 1em;
 }
 button {
-  margin-top: 20px;
+  margin-top: 1em;
   width: 10%;
   cursor: pointer;
 }
 p {
-  margin-top: 40px;
-  font-size: 13px;
+  margin-top: 4em;
+  font-size: 1em;
 }
 p a {
   text-decoration: underline;
