f9a8b9e0c77860e2796e9ff0d02d9bd945981027
Jaime Loeuf <jaimeloeuf@gmail.com>
2019-05-13 19:30:44 +0800

    Refactored out LogoutBtn from Notes view and more: 1.	Moved the logout button out of the notes view to seperate the logic.
    2.	Added in doc and todos.
    
    3.	Updated the view by adding in basic scoped CSS.

---
+++ src/components/LogoutBtn.vue
@@ -0,0 +1,23 @@
+// LogoutBtn.vue
+
+<template>
+  <button @click="logout">Logout</button>
+</template>
+
+<script>
+export default {
+  methods: {
+    name: "LogoutBtn",
+    logout: function() {
+      // Signout the current user and redirect to login page afterwards.
+      firebase
+        .auth()
+        .signOut()
+        .then(() => this.$router.replace("login"));
+    }
+  }
+};
+</script>
+
+<style scoped>
+</style>
--- src/views/Notes.vue
+++ src/views/Notes.vue
@@ -1,32 +1,33 @@
-/*
+/*  @Doc
+    Notes view that houses most of the components for showing all the notes and the note editor too.
 
     @Todo
+    - Fixed the notes list v-for loop to change the binding key. Else turn off ESLint.
     - Include a check if there is any notes. If there is no notes yet include a tutorial thing to help user get started.
 */
 
 <template>
-  <div class="notes">
+  <div class="notes-view">
     <img alt="Vue logo" src="@/assets/logo.png">
     <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
 
-    <button @click="logout">Logout</button>
+    <LogoutBtn/>
 
-    <ul>
-      <li v-for="note of notes" v-bind:key="note.text">{{ note.text }}</li>
+    <ul class="notes">
+      <li class="note" v-for="note of notes" v-bind:key="note.text">{{ note.text }}</li>
     </ul>
   </div>
 </template>
 
 <script>
-import firebase, { functions } from "firebase";
-
-import HelloWorld from "@/components/HelloWorld.vue";
+import LogoutBtn from "@/components/LogoutBtn.vue";
 
 export default {
   name: "notes",
-  //   components: {
-  //     HelloWorld
-  //   },
+  components: {
+    //   HelloWorld
+    LogoutBtn
+  },
   data() {
     return {
       notes: [
@@ -36,17 +37,16 @@ export default {
       ]
     };
   },
-  methods: {
-    logout: function() {
-      // Signout the current user and redirect to login page afterwards.
-      firebase
-        .auth()
-        .signOut()
-        .then(() => this.$router.replace("login"));
-    }
-  }
+  methods: {}
 };
 </script>
 
 <style scoped>
+.notes-view {
+  margin-top: 4em;
+}
+.notes {
+}
+.note {
+}
 </style>
