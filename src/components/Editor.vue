e95ffb7c94cff73b179be6bace8f3186ffd3e349
Jaime Loeuf <jaimeloeuf@gmail.com>
2019-05-14 21:53:59 +0800

    Created a new Editor component:     This is the editor component where user write their notes.

---
+++ src/components/Editor.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="editor">
+    <input type="text" v-model="title" placeholder="Title">
+    <br>
+    <!-- @Todo Implement autofocus -->
+    <input type="text" v-model="note" autofocus>
+    <br>
+
+    <!-- @Todo Create a new editor bar component -->
+    <button @click="close">Close</button>
+
+    <p>Hello</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Editor",
+//   props: {},
+  data() {
+    return {
+      title: "test",
+      note: "test"
+    };
+  },
+  methods: {
+    close: function() {
+      // Close the connection and close the editor
+    }
+  }
+};
+</script>
+
+<style>
+</style>
