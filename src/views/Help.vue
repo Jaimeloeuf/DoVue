0e662287af7eb6de431dedb5b921131b2431ff0c
Jaime Loeuf <jaimeloeuf@gmail.com>
2019-05-15 11:57:12 +0800

    Created a new Help view.

---
+++ src/views/Help.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="help">
+    <PublicNavbar/>
+    <h1>DoVue</h1>
+    <h3 class="slogan">Simple, Reliable, Beautiful</h3>
+    <img src="@/assets/logo.png" alt="DoVue logo">
+    <p class="description"></p>
+  </div>
+</template>
+
+
+<script>
+import PublicNavbar from "@/components/PublicNavbar.vue";
+
+export default {
+  name: "help",
+  components: {
+    PublicNavbar
+  }
+};
+</script>
+
+
+<style scoped>
+.help {
+  margin-top: 4em;
+}
+</style>
