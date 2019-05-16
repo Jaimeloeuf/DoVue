2117daf335308bf3c04537550c88df1a84eb6173
Jaime Loeuf <jaimeloeuf@gmail.com>
2019-05-16 09:31:09 +0800

    Created a new Contact view scaffold

---
+++ src/views/Contact.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="contact">
+    <PublicNavbar/>
+    <h1>DoVue</h1>
+    <h3 class="slogan">Simple, Reliable, Beautiful</h3>
+    <img src="@/assets/logo.png" alt="DoVue logo">
+    <p class="description"></p>
+  </div>
+</template>
+
+
+<script>
+import PublicNavbar from "@/components/PublicNavbar.vue";
+
+export default {
+  name: "contact",
+  components: {
+    PublicNavbar
+  }
+};
+</script>
+
+
+<style scoped>
+.contact {
+  margin-top: 4em;
+}
+</style>
