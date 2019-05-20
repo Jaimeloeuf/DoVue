8090edd501ac5c8e8f483ef1582b958816f902cf
Jaime Loeuf <jaimeloeuf@gmail.com>
2019-05-21 00:44:53 +0800

    Updated AboutModal style and layout: 1.  Added a basic CSS grid to put the content side by side.
    2.  Changed the layout of the modal slightly.

--- src/components/AboutModal.vue
+++ src/components/AboutModal.vue
@@ -1,24 +1,23 @@
-// AboutModal.vue
+/*
+
+    @Todo
+    - Create a share button to social media and stuff.
+    - Add the share button to About view and AboutModal
+*/
 
 <template>
-  <modal
-    name="AboutModal"
-    :adaptive="true"
-    :max-width="1000"
-    width="80%"
-    height="50%"
-  >
-    <div style="padding:30px; text-align: center">
-      <div class="about">
+  <modal name="AboutModal" :adaptive="true" :max-width="1000" width="80%" height="50%">
+    <div class="about">
+      <div class="left">
         <h1 style="color: #004f12;">This is DoVue</h1>
         <h3 class="slogan">Simple, Reliable, Beautiful</h3>
         <img src="@/assets/logo_big.png" alt="DoVue logo">
-        <div class="description">
-          <p>
-            A todo and notes taking app, focused to help you tackle whatever is on hand.
-            We believe in keeping things Simple and functional, and so reliable that you never want to go back to whatever you were using before.
-          </p>
-        </div>
+      </div>
+      <div class="description">
+        <p>
+          A todo and notes taking app, focused to help you tackle whatever is on hand.
+          We believe in keeping things Simple and functional, and so reliable that you never want to go back to whatever you were using before.
+        </p>
       </div>
     </div>
   </modal>
@@ -34,17 +33,24 @@ export default {
 
 <style scoped>
 .about {
-  margin-top: 4em;
-  /* Align left with 1em margin to align up with the Navigation bar */
+  /* Top and Bottom, 2em padding. Right and Left, 3em padding. */
+  padding: 2em 3em;
   text-align: left;
-  margin-left: 1em;
+  display: grid;
+  grid-template-columns: auto auto;
+  grid-gap: 3em;
 }
 
 .slogan {
   color: #0a8402;
 }
 
+.left {
+  grid-column: 1;
+}
+
 .description {
-  max-width: 40em;
+  margin-top: 1em;
+  grid-column: 2;
 }
 </style>
