d569502a19066186dbfc68dabde6dd9c305d1db1
Jaime Loeuf <jaimeloeuf@gmail.com>
2019-05-24 10:15:23 +0800

    Created Public view component:     Used for enclosing other public views

---
+++ src/views/Public.vue
@@ -0,0 +1,34 @@
+/*  @Doc
+    This is the Public view, which is the view component the client sees before logging in.
+    It is a enclosing view, meaning that it encloses other views that PUblic user is able to access.
+*/
+
+
+<template>
+  <div class="public-view">
+    <PublicNavbar/>
+
+    <!-- Router view for displaying modals via Routes -->
+    <router-view name="modal"/>
+
+    <!-- Default Router view for other views -->
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import PublicNavbar from "@/components/NavBars/PublicNavbar.vue";
+
+export default {
+  name: "Public",
+  components: {
+    PublicNavbar
+  }
+};
+</script>
+
+<style scoped>
+.public-view {
+  margin-top: 4em;
+}
+</style>
