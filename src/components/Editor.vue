a920195283a1729fc5e5e91736ac3ceaee76397e
Jaime Loeuf <jaimeloeuf@gmail.com>
2019-05-29 19:54:10 +0800

    Updated Editor component:     Working on finishing Editor component before integrating it in

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -1,21 +1,36 @@
-<template>
-  <div class="editor">
-    <input type="text" v-model="title" placeholder="Title">
-    <br>
-    <input v-autofocus type="text" v-model="note">
-    <br>
+/*  @Doc
+    This is the text editor for the notes.
 
-    <!-- @Todo Create a new editor bar component -->
-    <button @click="close">Close</button>
-  </div>
+    @Todo
+    - Make this into a modal component like keep
+
+    - Make all the li items for individaul notes work the same way as a button
+    - On click, trigger a function that will change the route to the route of the note,
+    - and show the EditorModal with the selected note in it using props passed into the editor component.
+*/
+
+<template>
+  <modal name="foo" height="auto" :scrollable="true">
+    <div class="editor">
+      <input type="text" v-model="title" placeholder="Title">
+      <br>
+      <textarea v-autofocus v-model="note" placeholder="Write your note here" cols="30" rows="10"/>
+      <br>
+
+      <p>no</p>
+
+      <!-- @Todo Create a new editor bar component -->
+      <button @click="close">Close</button>
+    </div>
+  </modal>
 </template>
 
 <script>
 export default {
   name: "Editor",
-//   props: {},
+  //   props: {},
 
-/*
+  /*
     Look at the router file.
     Open the note based on the note id in the route params
 */
