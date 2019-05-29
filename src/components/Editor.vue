36ae1307bf2c376375b1bf7b8ee4374032fd278f
Jaime Loeuf <jaimeloeuf@gmail.com>
2019-05-29 22:43:23 +0800

    Completed basic version of the Editor component:     It now works as a Modal that can be activated by a route.     The data used is still fake mock data, as the notes service has yet     to be integrated in yet.

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -2,25 +2,29 @@
     This is the text editor for the notes.
 
     @Todo
-    - Make this into a modal component like keep
-
     - Make all the li items for individaul notes work the same way as a button
-    - On click, trigger a function that will change the route to the route of the note,
-    - and show the EditorModal with the selected note in it using props passed into the editor component.
+    - On click, trigger a function that will change the route to the route of the note, and show the EditorModal with the selected note in it using props passed into the editor component.
 */
 
 <template>
-  <modal name="foo" height="auto" :scrollable="true">
+  <modal
+    name="editor-modal"
+    :adaptive="true"
+    height="auto"
+    width="60%"
+    :scrollable="true"
+    @before-close="beforeclose"
+  >
     <div class="editor">
       <input type="text" v-model="title" placeholder="Title">
       <br>
-      <textarea v-autofocus v-model="note" placeholder="Write your note here" cols="30" rows="10"/>
+      <textarea v-autofocus v-model="note" placeholder="Take a note here..." cols="30" rows="10"/>
       <br>
 
       <p>no</p>
 
       <!-- @Todo Create a new editor bar component -->
-      <button @click="close">Close</button>
+      <button @click="closeNsaveNote">Close</button>
     </div>
   </modal>
 </template>
@@ -28,23 +32,30 @@
 <script>
 export default {
   name: "Editor",
+
+  mounted() {
+    //   Show the modal once it is mounted onto the app
+    this.$modal.show("editor-modal");
+  },
+  methods: {
+    beforeclose() {
+      // @Todo Add a check for history to make sure that the previous route is still from DoVue
+      // Close modal and go back to previously show page
+      if (window.history.length > 1) this.$router.go(-1);
+      // Show the default notes/all page if there is no previous view.
+      // Applies to situation where the URL is bookmarked and opened directly
+      else this.$router.replace({ name: "all-notes" });
+    },
+    closeNsaveNote() {
+      // Close the connection and close the editor
+    }
+  },
   //   props: {},
-
-  /*
-    Look at the router file.
-    Open the note based on the note id in the route params
-*/
-
   data() {
     return {
       title: "test",
       note: "test"
     };
-  },
-  methods: {
-    close: function() {
-      // Close the connection and close the editor
-    }
   }
 };
 </script>
