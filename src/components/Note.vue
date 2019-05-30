07558678d2a9e6d4a76e5d45ad8e6c6ec8e7d31f
Jaime Loeuf <jaimeloeuf@gmail.com>
2019-05-30 23:47:23 +0800

    Updated Note component: 1.  Built out the basic layout.
    2.  Added base styles.
    
    3.  Added a check to make sure no more than 100 charaters are displayed.

--- src/components/Note.vue
+++ src/components/Note.vue
@@ -11,13 +11,20 @@
 */
 
 <template>
-  <li>
-      <!-- Display the text of the note -->
-    {{ note.text }}
-
+  <li class="note">
+    <!-- Display note title -->
+    <p class="note-title">{{ note.title }}</p>
+    <!-- Display the text of the note -->
+    <div class="note-text">
+      <p v-if="note.text.length < 100">{{ note.text }}</p>
+      <p v-else>
+        {{ note.text.substr(0, 100) }}
+        <span>...</span>
+      </p>
+    </div>
     <!-- @Todo Create a new note option component. With things like delete note / pin note buttons -->
     <!-- Upon clicking x to delete the note, event will be emitted for notes service to catch -->
-    <button @click="$emit('remove', note.id)">X</button>
+    <span @click="$emit('remove', note.id)">&times;</span>
   </li>
 </template>
 
@@ -31,3 +38,30 @@ export default {
   }
 };
 </script>
+
+<style scoped>
+.note {
+  /* @Todo This can be dynamically changed just like in keep where users can choose to view in blocks or list */
+  display: inline-block;
+  border: 1px solid #d7dee8;
+  border-radius: 0.8em;
+  padding: 0.5em;
+  margin: 0.5em;
+  width: 12em;
+  cursor: default;
+  /* @Todo build the hover effect in */
+}
+
+.note-title {
+    font-size: 1.1em;
+    font-weight: 500;
+    color: #545456;
+    text-align: left;
+    padding: 0em;
+    margin: 0em;
+    margin-top: 0.4em;
+}
+
+.note-text {
+}
+</style>
