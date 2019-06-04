c0bdb5adf7eb77d96971158c14ba33f0aef5aa26
Jaime Loeuf <jaimeloeuf@gmail.com>
2019-06-04 23:05:04 +0800

    Building a "unified" notes list/view component:     Building 1 single component that will handle generating the view for     the notes that meet the filter requirement as defined in the route.     Meaning regardless is users wants to see all notes or see notes with     a specific tag, it will all be rendered by this 1 component instead     of having multiple components to deal with notes filtered differently.
    1.  Deleted the tagged.vue component for viewing notes with a given tag.
    
    2.  all.vue will be converted into the NotesList component after completion
    
    3.  Updated the way all.vue component gets the notes and added props definition
    
    4.  Added docs on the filter in all.vue

--- src/views/Notes/all.vue
+++ src/views/Notes/all.vue
@@ -1,16 +1,37 @@
 /*  @Doc
-    View component to display all the user notes in the "notes" router-view of Notes view
-    View available at the path:
-        "/notes/all"
+    View component to display notes that are filtered based on route parameters, which are passed in as props.
+
+    Different props explained:
+        Filter will be a complex object with all the kv pairs.
+        Below is an example of the filter object.
+        {
+            tags: [],
+            archived: false,
+            deleted: false
+        }
+
+        Sort prop is also a complex object, but this is optional.
+        This object specifies how to sort the list of notes.
+        {
+            date_asc: false,
+            date_desc: true,
+        }
+    
 
     @Todo
+    - Change this view to be the "Notes" view, where the list of notes are always displayed through this view, and filtered in this view too. The filters can be passed in via route param props making them bookmarkable.
+    - Write the props from route code.
+    - Create function to filter notes by the given props.
+    - [See how to modify the array of notes](https://vuejs.org/v2/guide/list.html#Mutation-Methods)
     - Create notes service to replace hard coded mock data
     - Create a tutorial like component that help user get started if there is no notes.
 */
 
 <template>
-  <div class="all-notes">
-    <!-- @Todo What does @remove="removeTodo" event means? -->
+  <div class="notes">
+    <!-- <label>{{ filter.tags ? filter.tags : filter.type }}</label> -->
+
+    <!-- @Todo What does the @remove event do? -->
     <ul v-if="notes && notes.length" class="notes">
       <note v-for="note of notes" v-bind:key="note.id" :note="note"/>
     </ul>
@@ -29,9 +50,25 @@ export default {
   components: {
     note
   },
-  computed: mapState({
-    notes: state => state.notes
-  }),
+  props: {
+    filter: {
+      type: Object
+      // Filter off nothing by default making this view display all notes by default.
+    },
+    sort: {
+      type: Object,
+      default: undefined
+    }
+  },
+  computed: {
+    notes() {
+      // console.log(this.$props); // Print out the received props object
+      return this.$store.state.notes;
+    }
+  },
+  //   computed: mapState({
+  //     notes: state => state.notes
+  //   }),
   methods: {
     openNote() {}
   }
