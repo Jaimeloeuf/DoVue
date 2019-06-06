aa5d400b4e2ca346fd2cae883f175cf18d3ec2ce
Jaime Loeuf <jaimeloeuf@gmail.com>
2019-06-06 15:11:48 +0800

    Updating the tags bar component.

--- src/components/MenuBars/tags.vue
+++ src/components/MenuBars/tags.vue
@@ -1,6 +1,7 @@
 /*  @Doc
     This is the bar at the bottom of a note to display the tags the particular note has.
-    A list type of tags prop will be received to display
+    A list type of tags prop will be received to display.
+    This component is used in both the Note component and the editor component.
 
     @Todo
     - Make the note clickable to route user to the Notes view with the selected tag
@@ -9,13 +10,17 @@
 <template>
   <div v-if="tags && tags.length">
     <!-- <router-link v-for="tag of tags" v-bind:key="tag.id" class="tag"></router-link> -->
-    <p v-for="tag in tags" v-bind:key="tag" class="tag">{{ tag }}</p>
+    <div v-for="tag in tags" v-bind:key="tag" class="tag">
+      <span>{{ tag }}</span>
+      <!-- &times; is the cross button for removing the tag from that note -->
+      <span>&times;</span>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: "",
+  name: "tags_bar",
   props: ["tags"]
 };
 </script>
